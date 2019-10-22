f493e5aaf4f8b4c97a8fe5f31812cceba6d15e0b
xiangjg <1991887681@qq.com>
2019-10-22 11:23:09 +0800

    init

--- src/main/java/com/jone/record/entity/system/RoleEntity.java
+++ src/main/java/com/jone/record/entity/system/RoleEntity.java
@@ -19,11 +19,11 @@ public class RoleEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
