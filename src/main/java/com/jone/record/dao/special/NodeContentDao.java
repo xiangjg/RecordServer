2d6f7cdaf164cb0b0dbc7b81f85fde0041a42a88
xiangjg <1991887681@qq.com>
2019-11-04 17:18:16 +0800

    init

---
+++ src/main/java/com/jone/record/dao/special/NodeContentDao.java
@@ -0,0 +1,7 @@
+package com.jone.record.dao.special;
+
+import com.jone.record.entity.special.NodeContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NodeContentDao extends JpaRepository<NodeContent, Integer> {
+}
---
+++ src/main/java/com/jone/record/entity/special/NodeContent.java
@@ -0,0 +1,121 @@
+package com.jone.record.entity.special;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "t_qzt_node_content", schema = "public")
+public class NodeContent implements Serializable {
+    private Integer  id;
+    private Integer  nid;
+    private Integer  type;
+    private Integer  state;
+    private String name;
+    private String detail;
+    private String content;
+    private Integer  order;
+
+    @Basic
+    @Column(name = "id")
+    public Integer  getId() {
+        return id;
+    }
+
+    public void setId(Integer  id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "nid")
+    public Integer  getNid() {
+        return nid;
+    }
+
+    public void setNid(Integer  nid) {
+        this.nid = nid;
+    }
+
+    @Basic
+    @Column(name = "_type")
+    public Integer  getType() {
+        return type;
+    }
+
+    public void setType(Integer  type) {
+        this.type = type;
+    }
+
+    @Basic
+    @Column(name = "state")
+    public Integer  getState() {
+        return state;
+    }
+
+    public void setState(Integer  state) {
+        this.state = state;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "detail")
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Basic
+    @Column(name = "content")
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Basic
+    @Column(name = "order")
+    public Integer  getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer  order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NodeContent that = (NodeContent) o;
+        return id == that.id &&
+                nid == that.nid &&
+                type == that.type &&
+                state == that.state &&
+                order == that.order &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(detail, that.detail) &&
+                Objects.equals(content, that.content);
+    }
+
+    @Override
+    public int  hashCode() {
+        return Objects.hash(id, nid, type, state, name, detail, content, order);
+    }
+}
